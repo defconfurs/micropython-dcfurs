8aeaa137bfb7581a0c7bba258723782c77d11bcc
Owen Kirby <oskirby@gmail.com>
2018-06-09 17:47:53 -0700

    Tweaks to the LED matrix updates to reduce animation jitter.
    Some adjustments to the order of operations when updating the LED matrix
    improve playback by ensuring smoother transitions from one PWM level to
    another (ie: if the dimming steps are cleared partially through a timer
    callback). We also play around with Cortex-M bit-banding for performance.

--- extmod/dcfurs/matrix.c
+++ extmod/dcfurs/matrix.c
@@ -8,9 +8,13 @@
 #define DCF_PIN_COL_BANKA   0x01FE
 #define DCF_PIN_COL_BANKC   0x03FF
 
+#define DCF_BITBAND_SRAM(_addr_, _bit_) \
+    ((uint32_t *)SRAM1_BB_BASE)[(((unsigned long)_addr_) - SRAM1_BASE) * 8 + (_bit_)]
+
 struct dcf_rowdata {
-    uint32_t rowstrobe; /* Port B register value to activate the row. */   
-    uint32_t pxdata[DCF_DIMMING_STEPS];
+    uint32_t rowstrobe; /* Port B register value to activate the row. */
+    uint32_t pxenable;  /* Pixels that are enabled. */  
+    uint32_t pxdata[DCF_DIMMING_STEPS]; /* PWM Dimming levels */
 };
 
 struct dcf_framebuf {
@@ -20,6 +24,18 @@ struct dcf_framebuf {
 };
 
 static struct dcf_framebuf dcf_fb = {0};
+
+/* The fastest we can do to set a single pixel PWM intensity */
+static inline void
+dcf_set_pwm(struct dcf_rowdata *row, int x, int pwm)
+{
+    int i;
+    int step = ((unsigned int)pwm * DCF_DIMMING_STEPS + 128) / 256;
+    for (i = DCF_DIMMING_STEPS-1; i >= 0; i--) {
+        DCF_BITBAND_SRAM(&row->pxdata[i], x) = (i >= step);
+    }
+    DCF_BITBAND_SRAM(&row->pxenable, x) = (pwm != 0);
+}
 
 mp_obj_t dcfurs_matrix_init(void) {
     GPIO_InitTypeDef GPIO_InitStructure;
@@ -66,7 +82,6 @@ mp_obj_t dcfurs_matrix_init(void) {
 mp_obj_t dcfurs_matrix_loop(size_t n_args, const mp_obj_t *args)
 {
     /* Increment step, and blit the next dimming values */
-    dcf_fb.current_step++;
     if (dcf_fb.current_step >= DCF_DIMMING_STEPS) {
         struct dcf_rowdata *row;
 
@@ -80,8 +95,8 @@ mp_obj_t dcfurs_matrix_loop(size_t n_args, const mp_obj_t *args)
         GPIOB->BSRR = DCF_PIN_ROW_BANKB;
         GPIOA->BSRR = DCF_PIN_COL_BANKA;
         GPIOC->BSRR = DCF_PIN_COL_BANKC;
-        GPIOA->BSRR = (row->pxdata[0] & 0x3fc00) << 7;
-        GPIOC->BSRR = (row->pxdata[0] & 0x3ff) << 16;
+        GPIOA->BSRR = (row->pxenable & 0x3fc00) << 7;
+        GPIOC->BSRR = (row->pxenable & 0x3ff) << 16;
         GPIOB->BSRR = row->rowstrobe;
     }
     /* Switch pixels off when they reach their dimming level. */
@@ -91,6 +106,7 @@ mp_obj_t dcfurs_matrix_loop(size_t n_args, const mp_obj_t *args)
         GPIOC->BSRR = (pxdata & 0x3ff) >> 0;
     }
 
+    dcf_fb.current_step++;
     return mp_const_none;
 }
 
@@ -98,20 +114,17 @@ mp_obj_t dcfurs_set_row(mp_obj_t yobj, mp_obj_t data)
 {
     struct dcf_rowdata *row;
     int y = mp_obj_get_int(yobj);
-    int step;
     mp_buffer_info_t bufinfo;
     if ((y < 0) || (y >= DCF_TOTAL_ROWS)) {
         return mp_const_none; /* TODO: Throw a range error or something? */
     }
-    
-    /* Clear the row */
     row = &dcf_fb.rows[y];
-    for (step = 0; step < DCF_DIMMING_STEPS; step++) {
-        row->pxdata[step] = 0;
-    }
+    
     /* If an integer was provided, use it as a bitmap of pixels to switch on. */
     if (mp_obj_is_integer(data)) {
-        row->pxdata[0] = mp_obj_get_int(data);
+        row->pxenable = 0;
+        memset(row->pxdata, 0, sizeof(row->pxdata));
+        row->pxenable = mp_obj_get_int(data);
     }
     /* If a bytearray was provided, use it as an array of pixel intensities. */
     else if (mp_get_buffer(data, &bufinfo, MP_BUFFER_READ)) {
@@ -120,13 +133,10 @@ mp_obj_t dcfurs_set_row(mp_obj_t yobj, mp_obj_t data)
         if (bufinfo.len > DCF_TOTAL_COLS) {
             bufinfo.len = DCF_TOTAL_COLS;
         }
+
+        row->pxenable = 0;
         for (x = 0; x < bufinfo.len; x++) {
-            if (px[x] == 0) continue;
-            int step = ((unsigned int)px[x] * DCF_DIMMING_STEPS + 128) / 256;
-            if (step < DCF_DIMMING_STEPS) {
-                row->pxdata[step] |= (1 << x);
-            }
-            row->pxdata[0] |= (1 << x);
+            dcf_set_pwm(row, x, px[x]);
         }
     }
     return mp_const_none;
@@ -137,27 +147,23 @@ mp_obj_t dcfurs_set_pixel(mp_obj_t xobj, mp_obj_t yobj, mp_obj_t vobj)
     struct dcf_rowdata *row;
     int x = mp_obj_get_int(xobj);
     int y = mp_obj_get_int(yobj);
-    int step;
+
+    /* Sanity */
     if ((y < 0) || (y >= DCF_TOTAL_ROWS)) {
-        return mp_const_none; /* TODO: Throw a range error or something? */
+        return mp_const_none;
+    }
+    if ((x < 0) || (x >= DCF_TOTAL_COLS)) {
+        return mp_const_none;
     }
 
     /* Update the pixel dimming value. */
     row = &dcf_fb.rows[y];
-    for (step = 0; step < DCF_DIMMING_STEPS; step++) {
-        row->pxdata[step] &= ~(1 << x); /* Disable the pixel as we update it. */
-    }
-    if (mp_obj_is_true(vobj)) {
-        row->pxdata[0] |= (1 << x);     /* Enable full intensity. */
-    }
-
-    /* Set a dimming intensity by configuring at which step we turn the pixel off. */
+    row->pxenable &= ~(1 << x);
     if (mp_obj_is_integer(vobj)) {
-        step = (mp_obj_get_int(vobj) * DCF_DIMMING_STEPS + 128) / 256;
-        if (step == 0) step = 1;
-        if (step < DCF_DIMMING_STEPS) {
-            row->pxdata[step] |= (1 << x);
-        }
+        dcf_set_pwm(row, x, mp_obj_get_int(vobj));
+    }
+    else if (mp_obj_is_true(vobj)) {
+        dcf_set_pwm(row, x, 256);
     }
     return mp_const_none;
 }
@@ -211,7 +217,7 @@ mp_obj_t dcfurs_clear(void)
 {
     int i;
     for (i = 0; i < DCF_TOTAL_ROWS; i++) {
-        memset(dcf_fb.rows[i].pxdata, 0, sizeof(dcf_fb.rows[i].pxdata));
+        dcf_fb.rows[i].pxenable = 0;
     }
     return mp_const_none;
 }
